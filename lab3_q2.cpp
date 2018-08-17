8a566bb13454fcdf697d19e2c373e50bad7bf3e7
debesh20000 <42192991+debesh20000@users.noreply.github.com>
2018-08-17 11:14:12 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,46 @@
+//first include the library
+#include <iostream>
+using namespace std;
+//write the main function
+int main()
+{
+//declaration of variables
+int a,b;
+int result;
+a=10;
+b=5;
+//sum of variables
+result= a+b;
+cout << result <<endl;
+//differnce of variables
+result= a-b;
+cout << result <<endl;
+//multiplication of variables
+result = a*b;
+cout << result <<endl;
+//division of variables
+result = a/b;
+cout << result <<endl;
+//modulus of variables
+result = a%b;
+cout << result <<endl;
+//declaration of variables
+float c, d;
+float results;
+c=12.6;
+d=6.3;
+//sum of variables
+results= c+d;
+cout << results <<endl;
+//differnce of variables
+results= c-d;
+cout << results <<endl;
+//multiplication of variables
+results = c*d;
+cout << results <<endl;
+//division of variables
+results = c/d;
+cout << results <<endl;
+return 0;
+}
+
