54f8e575a0b0c9b3c5fa8161c324fd93f5577808
debesh20000 <42192991+debesh20000@users.noreply.github.com>
2018-08-17 09:11:53 +0530

    Add files via upload

---
+++ lab2q2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "What is your name"
+read DEBESH
+mkdir $DEBESH
+touch $DEBESH/$DEBESH"1".cpp $DEBESH/$DEBESH"2".cpp $DEBESH/$DEBESH"3".cpp $DEBESH/$DEBESH"4".cpp $DEBESH/$DEBESH"5".cpp
